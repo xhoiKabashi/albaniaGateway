bfd2b8f952b4984af7a451bf1d0f5b8fcfc59e3e
Xhokabash <xhokabash@gmail.com>
2024-06-08 19:29:21 +0200

    add cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    domains: ["https://albaniagateway.com"], // Replace with your domains
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+  },
+};
 
 export default nextConfig;
