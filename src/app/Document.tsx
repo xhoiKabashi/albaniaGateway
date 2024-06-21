a381275d29cd0ee0e1058f752b1cfc6d185a2454
Xhokabash <xhokabash@gmail.com>
2024-06-21 21:26:44 +0200

    add

---
+++ src/app/Document.tsx
@@ -0,0 +1,11 @@
+import Script from "next/script";
+
+const Ads = () => {
+  return (
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8349642777565149"
+      crossOrigin="anonymous"
+    ></Script>
+  );
+};
