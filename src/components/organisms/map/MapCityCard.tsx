a04b3e4da15868a040a07b540fdd161f7affe2ed
Xhokabash <xhokabash@gmail.com>
2024-08-01 15:38:46 +0200

    add

--- src/components/organisms/map/MapCityCard.tsx
+++ src/components/organisms/map/MapCityCard.tsx
@@ -28,21 +28,21 @@ const MapCityCard = ({ city }: CityCardProps) => {
   const cityInfo: any = {
     Tropoja: {
       text: "Tropoja",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Tropoja",
       imageUrl: tropoje,
       href: "/explore-cities/tropoja",
       away: "247",
     },
     Theth: {
       text: "Theth",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Theth",
       imageUrl: theth,
       href: "/explore-cities/theth",
       away: "177",
     },
     Shkoder: {
       text: "Shkodra",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Shkodra",
       imageUrl: shkoder,
       href: "/explore-cities/shkodra",
       away: "101",
@@ -55,55 +55,55 @@ const MapCityCard = ({ city }: CityCardProps) => {
     },
     Kruje: {
       text: "Kruja",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Kruja",
       imageUrl: kruje,
       href: "/explore-cities/kruja",
       away: "35",
     },
-    Tirane: {
+    Tirana: {
       text: "Tirana",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Tirana",
       imageUrl: tirana,
       href: "/explore-cities/tirana",
     },
     Durres: {
       text: "Durres",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Durres",
       imageUrl: durres,
       href: "/explore-cities/durres",
       away: "36",
     },
     Elbasan: {
       text: "Elbasan",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Elbasan",
       imageUrl: elbasani,
       href: "/explore-cities/elbasan",
       away: "42 ",
     },
     Pogradec: {
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
       text: "Pogradec",
+      alt: "Image of Pogradec",
       imageUrl: pogradec,
       href: "/explore-cities/pogradec",
       away: "126 ",
     },
     Korca: {
       text: "Korca",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Korca",
       imageUrl: korca,
       href: "/explore-cities/korca",
       away: "166 ",
     },
     Berat: {
       text: "Berat",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Berat",
       imageUrl: berat,
       href: "/explore-cities/berat",
       away: "101 ",
     },
     Vlora: {
       text: "Vlora",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Vlora",
       imageUrl: vlora,
       href: "/explore-cities/vlora",
       away: "160",
@@ -112,40 +112,40 @@ const MapCityCard = ({ city }: CityCardProps) => {
       text: "Dhermi & Himara",
       imageUrl: dhermi,
       href: "/explore-cities/himara-dhermi",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Dhermi & Himara",
       away: "240",
     },
     Tepelena: {
       text: "Tepelena",
       imageUrl: tepelena,
       href: "/explore-cities/tepelena",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Tepelena",
       away: "205",
     },
     Saranda: {
       text: "Saranda",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Saranda",
       imageUrl: saranda,
       href: "/explore-cities/saranda",
       away: "265",
     },
     Gjirokaster: {
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
       text: "Gjirokastra",
-      href: "/explore-cities/gjirokastra",
+      alt: "Image of Gjirokastra",
       imageUrl: gjirokaster,
+      href: "/explore-cities/gjirokastra",
       away: "235",
     },
     Permet: {
       text: "Permet",
       imageUrl: permet,
       href: "/explore-cities/permet",
-      alt: "Image of the Gjirokastër Castle, in focus is the Albanian flag",
+      alt: "Image of Permet",
       away: "248",
     },
   };
 
-  const info = city ? cityInfo[city] : cityInfo["Tirane"];
+  const info = city ? cityInfo[city] : cityInfo["Tirana"];
 
   if (!info) {
     return null;
@@ -153,32 +153,31 @@ const MapCityCard = ({ city }: CityCardProps) => {
 
   return (
     <div className="bg-sky-20 flex flex-col justify-center items-center rounded-md mb-10  scale-90 md:scale-100">
-      <div className="relative  h-72 w-80 overflow-hidden">
+      <div className="relative h-72 w-80 overflow-hidden">
         <Image
           src={info.imageUrl}
           alt={"Image of " + info.text}
           className="w-full rounded-md bg-center"
           layout="fill"
-          objectFit="cover" // Ensures the image fits well
+          objectFit="cover"
           priority={true}
           sizes="(min-width: 808px) 50vw, 100vw"
           placeholder="blur"
         />
-        <div className="absolute inset-0 bg-black  opacity-35 rounded-md"></div>
+        <div className="absolute inset-0 bg-black opacity-35 rounded-md"></div>
       </div>
-      <div className="relative  bottom-52  md:bottom-64 text-white px-3 text-sm flex gap-2 items-center">
-        {info.text === "Tirane"
-          ? `Just 30 km away from the airport`
+      <div className="relative bottom-52 md:bottom-64 text-white px-3 text-sm flex gap-2 items-center">
+        {info.text == "Tirana"
+          ? "Just 30 km away from the airport"
           : `Just ${info.away} km away from Tirana`}
-
-        <FaMapPin className=" text-red-600" />
+        <FaMapPin className="text-red-600" />
       </div>
-      <div className="relative bottom-16  font-bold text-white px-3 text uppercase  tracking-wider">
+      <div className="relative bottom-16 font-bold text-white px-3 text-uppercase tracking-wider">
         Explore: {info.text}
       </div>
       <Link
         href={info.href}
-        className=" bg-sky-500 py-1  w-60 text-xs  px-3 rounded-lg text-white cursor-pointer  active:scale-95 text-center"
+        className="bg-sky-500 py-1 w-60 text-xs px-3 rounded-lg text-white cursor-pointer active:scale-95 text-center"
       >
         Read more about {info.text}
       </Link>
